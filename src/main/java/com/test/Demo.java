9ed3d49c0b83309f2bfec4ca38fc7b2dd304967b
liuyu2837 <283759907@qq.com>
2019-10-23 15:21:05 +0800

    1.1

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -5,6 +5,6 @@ public class Demo {
     public static void main(String[] args){
         System.out.println(" ----------- github ");
 
-        System.out.println(" ----------- github");
+        System.out.println(" ----------- github2");
     }
 }
